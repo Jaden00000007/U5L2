3aaf7152aa79c23eb451e4c4ed6e0280a4e40e5a
Jaden00000007 <146095161+Jaden00000007@users.noreply.github.com>
2025-01-10 15:34:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* === Imports === */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
 
 /* === Firebase Setup === */
 const firebaseConfig = {
@@ -19,6 +23,8 @@ console.log(app.options.projectId)
 
 /* == UI - Elements == */
 
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+
 const viewLoggedOut = document.getElementById("logged-out-view")
 const viewLoggedIn = document.getElementById("logged-in-view")
 
@@ -30,6 +36,8 @@ const passwordInputEl = document.getElementById("password-input")
 const signInButtonEl = document.getElementById("sign-in-btn")
 const createAccountButtonEl = document.getElementById("create-account-btn")
 
+const signOutButtonEl = document.getElementById("sign-out-btn")
+
 /* == UI - Event Listeners == */
 
 signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
@@ -37,64 +45,150 @@ signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 signInButtonEl.addEventListener("click", authSignInWithEmail)
 createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
 
+signOutButtonEl.addEventListener("click", authSignOut)
 /* === Main Code === */
 
 showLoggedOutView()
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+    const uid = user.uid;
+    showLoggedInView()
+  } else {
+    showLoggedOutView
+  }
+});
 
 /* === Functions === */
 
 /* = Functions - Firebase - Authentication = */
 
-function authSignInWithGoogle() {
-    console.log("Sign in with Google")
-}
+function showProfilePicture(imgElement, user) {
+    if (user.photoURL) {
+        imgElement.src = user.photoURL
+    }
+    else{
+        imgElement.src = "assets/images/defaultPic.jpg"
+    }
+ }
+ 
+ function showUserGreeting(element, user) {
+    /*  Challenge:
+        Use the documentation to make this function work.
+       
+        This function has two parameters: element and user
+       
+        We will call this function inside of onAuthStateChanged when the user is logged in.
+       
+        The function will be called with the following arguments:
+        showUserGreeting(userGreetingEl, user)
+       
+        If the user has a display name, then set the textContent of element to:
+        "Hi ___ ( your first name)"
+        Where __ is replaced with the actual first name of the user
+       
+        Otherwise, set the textContent of element to:
+        "Hey friend, how are you?"
+    */
+        if (user.displayName) {
+            const firstName = user.displayName.split(" ")[0];
+            element.textContent = `Hi ${firstName}`;
+        } 
+        else {
+            element.textContent = "Hi Guest";
+        }
+ }
+ 
+
+function authSignOut() {
+    /*  Challenge:
+         Import the signOut function from 'firebase/auth'
+ 
+ 
+        Use the code from the documentation to make this function work.
+   
+        If the log out is successful then you should show the logged out view using showLoggedOutView()
+        If something went wrong, then you should log the error message using console.error.
+    */
+    const auth = getAuth();
+    signOut(auth).then(() => {
+    console.log("User sign out successful")
+    showLoggedOutView()
+    }).catch((error) => {
+    console.error(error.message)
+    });
+ }
+ 
 
 function authSignInWithEmail() {
     console.log("Sign in with email and password")
+    /*  Challenge:
+  1  Import the signInWithEmailAndPassword function from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js"
+  2 Use the code from the documentation to make this function work.
+  3  Make sure to first create two consts, 'email' and 'password', to fetch the values from the input fields emailInputEl and passwordInputEl.
+   4 If the login is successful then you should show the logged in view using showLoggedInView()
+   5   If something went wrong, then you should log the error message using console.error.
+    */
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        showLoggedInView()
+        // ...
+      })
+      .catch((error) => {
+        console.error(error.message)
+      });
 }
+
 
 function authCreateAccountWithEmail() {
     console.log("Sign up with email and password")
         /*  Challenge:
-        1 Import the createUserWithEmailAndPassword function from from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-       2 Use the code from the documentation to make this function work.
-       3 Make sure to first create two consts, 'email' and 'password', to fetch the values from the input fields emailInputEl and passwordInputEl.
-       4 If the creation of user is successful then you should show the logged in view using showLoggedInView()
-       5 If something went wrong, then you should log the error message using console.error.
+    1 Import the createUserWithEmailAndPassword function from 'firebase/auth'
+    2 Use the code from the documentation to make this function work.
+    3 Make sure to first create two consts, 'email' and 'password', to fetch the values from the input fields emailInputEl and passwordInputEl.
+    4 If the creation of user is successful then you should show the logged in view using showLoggedInView()
+    5 If something went wrong, then you should log the error message using console.error.
     */
-        const email = emailInputEl.value
-        const password = passwordInputEl.value
+    const email = emailInputEl.value
+    const password= passwordInputEl.value
 
-       createUserWithEmailAndPassword(auth, email, password)
-         .then((userCredential) => {
-           // Signed up 
-           showLoggedInView()
-           // ...
-         })
-         .catch((error) => {
-           console.error(error.message)
-           // ..
-         });
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed up
+        showLoggedInView()
+    })
+    .catch((error) => {
+    console.error(error.message)
+  });
+} // of authCreateAccountWithEmail()
+
+
+function authSignInWithGoogle() {
+    console.log("Sign in with Google")
 }
-
 
 /* == Functions - UI Functions == */
+
 function showLoggedOutView() {
-    hideElement(viewLoggedIn)
-    showElement(viewLoggedOut)
-}
-
-function showLoggedInView() {
-    hideElement(viewLoggedOut)
-    showElement(viewLoggedIn)
-}
-
-function showElement(element) {
-    element.style.display = "flex"
-}
-
-function hideElement(element) {
-    element.style.display = "none"
-}
-
+    hideView(viewLoggedIn)
+    showView(viewLoggedOut)
+ }
+ 
+ function showLoggedInView() {
+    hideView(viewLoggedOut)
+    showView(viewLoggedIn)
+ }
+ 
+ function showView(view) {
+    view.style.display = "flex"
+ } 
+ 
+ function hideView(view) {
+    view.style.display = "none"
+ }
+ 
 //credit: coursera
